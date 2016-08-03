ffff03349be82847ae887db331cac5fc0414c21d
tzf <282629379@qq.com>
2016-08-03 17:10:35 +0800

    将Ch340项目转换成为了project 方便下载

---
+++ app/src/main/java/com/tzf/Main.java
@@ -0,0 +1,47 @@
+package com.tzf;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.won.usb_ch340.MainActivity;
+import com.won.usb_ch340.R;
+
+/**
+ * Created by Tang on 2016/8/1.
+ */
+public class Main extends Activity {
+    Button btn_TJWD;
+    Button btn_MonitoringControl;
+    Button btn_Setting;
+    public final String TAG="tzf";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        initView();
+    }
+
+    private void initView() {
+        btn_MonitoringControl= (Button) findViewById(R.id.btn_MonitoringControl);
+        btn_TJWD= (Button) findViewById(R.id.btn_InitTimeJW);
+        btn_TJWD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(Main.this, "----------进入MainActivity-----------", Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "onClick: 进入MainActivity");
+                Main.this.startActivity(new Intent(Main.this,MainActivity.class));
+            }
+        });
+        btn_Setting= (Button) findViewById(R.id.btn_Setting);
+
+    }
+
+
+}
