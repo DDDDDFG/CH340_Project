db03056b86f0d1d06996ac847fe4573c8ae1febe
大大大大峰哥 <282629379@qq.com>
2016-08-15 17:23:04 +0800

    删除了GetNet  同时将APP中与Agent代理进行了连接

--- app/src/main/java/com/tzf/Main.java
+++ app/src/main/java/com/tzf/Main.java
@@ -7,7 +7,6 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.won.usb_ch340.MainActivity;
 import com.won.usb_ch340.R;
@@ -24,7 +23,7 @@ public class Main extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main1);
         initView();
     }
 
